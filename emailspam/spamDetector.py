1688b5493aa01bace8eaca318af52b888d267e3f
22r01a6723 <161785732+22r01a6723@users.noreply.github.com>
2024-12-10 18:22:56 +0530

    Update spamDetector.py

--- emailspam/spamDetector.py
+++ emailspam/spamDetector.py
@@ -1,24 +1,52 @@
 import streamlit as st
 import pickle
 
-model = pickle.load(open('spam.pkl','rb'))
-cv = pickle.load(open('vec.pkl','rb'))
+# Load the model and vectorizer
+model = pickle.load(open('spam.pkl', 'rb'))
+cv = pickle.load(open('vec.pkl', 'rb'))
 
+# Main application
 def main():
-	st.title("Email Spam Classification Application")
-	st.write("This is a Machine Learning application to classify emails as spam or ham.")
-	st.subheader("Classification")
-	user_input=st.text_area("Enter an email to classify" ,height=150)
-	if st.button("Classify"):
-		if user_input:
-			data=[user_input]
-			print(data)
-			vec=cv.transform(data).toarray()
-			result=model.predict(vec)
-			if result[0]==0:
-				st.success("This is Not A Spam Email")
-			else:
-				st.error("This is A Spam Email")
-		else:
-			st.write("Please enter an email to classify.")
-main()
+    # Title and header styling
+    st.markdown("<h1 style='text-align: center; color: #4CAF50;'>Email Spam Classification</h1>", unsafe_allow_html=True)
+    st.markdown("""
+        <p style="text-align: center; color: #555555; font-size: 18px;">
+        This is a Machine Learning application to classify emails as spam or ham.
+        </p>
+    """, unsafe_allow_html=True)
+    
+    # Add some spacing
+    st.markdown("<br><br>", unsafe_allow_html=True)
+
+    # Input text area for email
+    st.subheader("Enter Email Text Below")
+    user_input = st.text_area("Email Content", height=200, placeholder="Type or paste an email here...")
+
+    # Classify button with some styling
+    classify_button = st.button("Classify Email", key="classify_btn", help="Click to classify the entered email")
+
+    # Displaying the result
+    if classify_button:
+        if user_input:
+            data = [user_input]
+            vec = cv.transform(data).toarray()
+            result = model.predict(vec)
+            
+            if result[0] == 0:
+                st.markdown("<h4 style='color: #4CAF50;'>This is Not A Spam Email</h4>", unsafe_allow_html=True)
+            else:
+                st.markdown("<h4 style='color: #FF5733;'>This is A Spam Email</h4>", unsafe_allow_html=True)
+        else:
+            st.warning("Please enter some text to classify.")
+
+    # Footer for a nice touch
+    st.markdown("""
+        <hr>
+        <p style="text-align: center; color: #888888; font-size: 12px;">
+        Powered by Streamlit & Machine Learning | Developed by You
+        </p>
+    """, unsafe_allow_html=True)
+
+# Run the app
+if __name__ == '__main__':
+    main()
